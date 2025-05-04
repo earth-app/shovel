4e9ea7a631a3d5257efe3c858be12c92b7031b05
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-05-03 20:15:11 -0500

    let's try this

--- src/apple/main/com/earthapp/shovel/http.apple.kt
+++ src/apple/main/com/earthapp/shovel/http.apple.kt
@@ -2,9 +2,17 @@ package com.earthapp.shovel
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import platform.Foundation.*
+import platform.Security.*
 
+@OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
 actual val engine: HttpClientEngine = Darwin.create {
     pipelining = true
     dispatcher = Dispatchers.IO.limitedParallelism(PARALLEL_COUNT)
@@ -15,5 +23,20 @@ actual val engine: HttpClientEngine = Darwin.create {
 
     configureSession {
         setAllowsCellularAccess(true)
+    }
+
+    handleChallenge { session, task, challenge, completionHandler ->
+        val serverTrust = challenge.protectionSpace.serverTrust ?: run {
+            completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, null)
+            return@handleChallenge
+        }
+
+         memScoped {
+            val resultVar = alloc<SecTrustResultTypeVar>()
+            SecTrustEvaluate(serverTrust, resultVar.ptr)
+        }
+
+        val credential = NSURLCredential.credentialForTrust(serverTrust)
+        completionHandler(NSURLSessionAuthChallengeUseCredential, credential)
     }
 }
